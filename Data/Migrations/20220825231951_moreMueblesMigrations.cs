92f2c29aa734b5d070f5a740893b0c21d0f0392a
DiegoZamalloa <diegojzp15@gmail.com>
2022-08-25 18:23:55 -0500

    imagenmuebles1.0

---
+++ Data/Migrations/20220825231951_moreMueblesMigrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppMuebles.Data.Migrations
+{
+    public partial class moreMueblesMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
