8205f55113e4b7072a50206fcf4770d4a389494b
DiegoZP1 <diegojzp15@gmail.com>
2022-10-06 10:15:16 -0500

    Filtros para el catalogo

--- Controllers/MueblesController.cs
+++ Controllers/MueblesController.cs
@@ -28,7 +28,9 @@ namespace AppMuebles.Controllers
             _userManager = userManager;
         }
 
-         public async Task<IActionResult> Index(string? searchString)
+
+            [HttpGet]
+         public async Task<IActionResult> Index(string? searchString ,string? Search ,string? Color,string? Categoria)
         {
             
             var muebles = from o in _context.DataMuebles select o;
@@ -37,10 +39,32 @@ namespace AppMuebles.Controllers
                 muebles = muebles.Where(s => s.Nombre.Contains(searchString)); //Algebra de bool
                 // & + WHERE name like '%ABC%'
             }
+
+            if(!String.IsNullOrEmpty(Search)){
+                muebles = muebles.Where(s => s.EstadoMue.Contains(Search)); //Algebra de bool
+                // & + WHERE name like '%ABC%'
+            }
+
+
+            if(!String.IsNullOrEmpty(Color)){
+                muebles = muebles.Where(s => s.color.Contains(Color)); //Algebra de bool
+                // & + WHERE name like '%ABC%'
+            }
+
+            if(!String.IsNullOrEmpty(Categoria)){
+                muebles = muebles.Where(s => s.categoria.Contains(Categoria)); //Algebra de bool
+                // & + WHERE name like '%ABC%'
+            }
+
             muebles = muebles.Where(s => s.Status.Contains("Activo"));
             
             return View(await muebles.ToListAsync());
         }
+
+
+        
+        
+
 
          public async Task<IActionResult> Details(int? id)
         {
